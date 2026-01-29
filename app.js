0c27e98a3c7e3ff62837ad8d5c24ba55977b921c
NitheshHarman <112096629+NitheshHarman@users.noreply.github.com>
2026-01-29 16:08:48 +0530

    [React initial commit]

---
+++ app.js
@@ -0,0 +1,10 @@
+const parent = React.createElement(
+  'div',
+  { id: 'root' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', { key: 'header' }, 'Header Text'),
+    React.createElement('h2', { key: 'sub-header' }, 'Subheader Text'),
+  ]),
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
